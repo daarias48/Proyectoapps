822a676cbd6758cdbd05787f02760c5712d3210c
Angie <angiecaronga@yahoo.es>
2020-10-03 17:21:28 -0500

    Pantalla inicio no completo

---
+++ app/src/main/java/com/example/proyecto/Pantalla_inicio.java
@@ -0,0 +1,14 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla_inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicio);
+    }
+}
