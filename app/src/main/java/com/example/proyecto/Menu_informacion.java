372f330a1deefb63972fea0cebfbeb402c30c471
Angie <angiecaronga@yahoo.es>
2020-10-11 13:45:16 -0500

    Agregué dos interfaces

---
+++ app/src/main/java/com/example/proyecto/Menu_informacion.java
@@ -0,0 +1,27 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Menu_informacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_informacion);
+    }
+
+
+    public void  Ansiedad (View view){
+        Intent ans= new Intent(this,Info_ansiedad.class);
+        startActivity(ans);
+    }
+
+    public void  Anterior (View view){
+        Intent ant= new Intent(this,Pantalla_inicio.class);
+        startActivity(ant);
+    }
+}
--- app/src/main/java/com/example/proyecto/Pantalla_inicio.java
+++ app/src/main/java/com/example/proyecto/Pantalla_inicio.java
@@ -2,7 +2,9 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Pantalla_inicio extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Pantalla_inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_inicio);
+    }
+
+    public void  Menuinfo (View view){
+        Intent inf= new Intent(this,Menu_informacion.class);
+        startActivity(inf);
     }
 }
