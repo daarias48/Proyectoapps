093ac681abfbfeb31202f78205978bbcf9a25d6b
Angie <angiecaronga@yahoo.es>
2020-10-13 09:20:36 -0500

    Pantalla info estres

--- app/src/main/java/com/example/proyecto/Info_estres.java
+++ app/src/main/java/com/example/proyecto/Info_estres.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Info_ansiedad extends AppCompatActivity {
+public class Info_estres extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info_ansiedad);
+        setContentView(R.layout.activity_info_estres);
     }
 
     public void  Anterior (View view){
