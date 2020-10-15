41ff2a564a4183294f5751cffcfb6b7776c9034c
Angie <angiecaronga@yahoo.es>
2020-10-15 12:41:43 -0500

    Una interfaz

--- app/src/main/java/com/example/proyecto/Ejer_identificando.java
+++ app/src/main/java/com/example/proyecto/Ejer_identificando.java
@@ -2,7 +2,9 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Ejer_identificando extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class Ejer_identificando extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejer_identificando);
     }
+
+    public void  Diario (View view){
+        Intent diar= new Intent(this,Diario_R.class);
+        startActivity(diar);
+    }
+
 }
